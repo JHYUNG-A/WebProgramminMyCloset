cae8b7cd55b0b605c0485ee7a4f9820842ed067d
MIN0225 <cro77@naver.com>
2023-05-31 21:37:26 +0900

    feat:내 옷장 페이지 내 옷 추가, 페이징 구현

--- MyCloset/src/pages/MyClothes.jsx
+++ MyCloset/src/pages/MyClothes.jsx
@@ -1,13 +1,90 @@
 // 김준하
 // 내 옷장 페이지
-import React from 'react'
+import {useState, useEffect } from 'react'
 import Navbar from '../components/Navbar/Navbar'
 
 function MyClothes() {
+
+  const [item, setItem] = useState({
+    name: '',
+    type: '',
+    purchaseDate: '',
+    size: '',
+    price: '',
+    image: '',
+  });
+
+  const [clothes, setClothes] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 8;
+
+
+  useEffect(() => {
+    const storedClothes = JSON.parse(localStorage.getItem('clothes')) || [];
+    setClothes(storedClothes);
+  }, []);
+
+
+  const handleChange = (event) => {
+    setItem({
+      ...item,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   const clothes = JSON.parse(localStorage.getItem('clothes')) || [];
+  //   clothes.push(item);
+  //   localStorage.setItem('clothes', JSON.stringify(clothes));
+  //   alert('Item added!');
+  // };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newClothes = [...clothes, item];
+    localStorage.setItem('clothes', JSON.stringify(newClothes));
+    setClothes(newClothes);
+    alert('Item added!');
+  };
+
+  const handlePrevious = () => {
+    setCurrentPage((prev) => Math.max(prev - 1, 1));
+  };
+
+  const handleNext = () => {
+    setCurrentPage((prev) => Math.min(prev + 1, Math.ceil(clothes.length / itemsPerPage)));
+  };
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const currentClothes = clothes.slice(startIndex, endIndex);
+
+
   return (
     <div>
       <h1>MyClothes</h1>
       <Navbar />
+      <h1>Add Clothes</h1>
+      <form onSubmit={handleSubmit}>
+        <input name="name" placeholder="Name" onChange={handleChange} />
+        <input name="type" placeholder="Type" onChange={handleChange} />
+        <input name="purchaseDate" placeholder="Purchase Date" onChange={handleChange} />
+        <input name="size" placeholder="Size" onChange={handleChange} />
+        <input name="price" placeholder="Price" onChange={handleChange} />
+        <input name="image" placeholder="Image URL" onChange={handleChange} />
+        <button type="submit">Add</button>
+      </form>
+      <h1>My Clothes</h1>
+      {currentClothes.map((clothe, index) => (
+        <div key={index}>
+          <img src={clothe.image} alt={clothe.name} />
+          <h2>{clothe.name}</h2>
+          <p>{clothe.price}</p>
+        </div>
+      ))}
+      <button onClick={handlePrevious}>Previous</button>
+      <button onClick={handleNext}>Next</button>
     </div>
   )
 }
