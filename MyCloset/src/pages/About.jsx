f38f2929f3f1c5df93fcf6dee0ba24c1a4d04a3b
MIN0225 <cro77@naver.com>
2023-06-17 21:11:58 +0900

    feat: 개발자 소개 페이지

--- MyCloset/src/pages/About.jsx
+++ MyCloset/src/pages/About.jsx
@@ -1,11 +1,50 @@
 import React from 'react'
+import { DeveloperContainer, DeveloperCard, DeveloperImage, DeveloperInfo, DeveloperName, DeveloperRole, DeveloperMajor, DeveloperID } from './About.style';
+import image1 from '../assets/안재형.png';
+import image2 from '../assets/김준하.png';
+import image3 from '../assets/김광민.png';
+
 
 function About() {
+  const developers = [
+    {
+      name: '안재형',
+      major: '전자전기공학부',
+      id: '20210001',
+      image: image1,
+      role: 'Frontend Developer',
+    },
+    {
+      name: '김준하',
+      major: '컴퓨터공학과',
+      id: '20210002',
+      image: image2,
+      role: 'Backend Developer',
+    },
+    {
+      name: '김광민',
+      major: '멀티미디어공학과',
+      id: '20210003',
+      image: image3,
+      role: 'Full Stack Developer',
+    },
+  ];
+
   return (
-    <div>
-      <h1>About</h1>
-    </div>
+    <DeveloperContainer>
+      {developers.map((developer, index) => (
+        <DeveloperCard key={index}>
+          <DeveloperImage src={developer.image} alt={developer.name} />
+          <DeveloperInfo>
+            <DeveloperName>{developer.name}</DeveloperName>
+            <DeveloperMajor>{developer.major}</DeveloperMajor>
+            <DeveloperID>{developer.id}</DeveloperID>
+            <DeveloperRole>{developer.role}</DeveloperRole>
+          </DeveloperInfo>
+        </DeveloperCard>
+      ))}
+    </DeveloperContainer>
   )
 }
 
-export default About
+export default About;
---
+++ MyCloset/src/pages/About.style.jsx
@@ -0,0 +1,49 @@
+import styled from 'styled-components';
+
+export const DeveloperContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  padding: 20px;
+`;
+
+export const DeveloperCard = styled.div`
+  width: 300px;
+  border: 1px solid #ddd;
+  border-radius: 10px;
+  margin: 20px;
+  padding: 20px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+`;
+
+export const DeveloperImage = styled.img`
+  width: 100%;
+  height: 200px;
+  object-fit: cover;
+  border-radius: 10px;
+`;
+
+export const DeveloperInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 20px;
+`;
+
+export const DeveloperName = styled.h2`
+  margin: 0;
+`;
+
+export const DeveloperMajor = styled.p`
+  margin: 5px 0;
+`;
+
+export const DeveloperID = styled.p`
+  margin: 5px 0;
+`;
+
+export const DeveloperRole = styled.p`
+  margin: 5px0;
+  font-weight: bold;
+  color: #007BFF;
+`;
