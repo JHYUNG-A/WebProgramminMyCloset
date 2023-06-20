f538cbcc621e7e3d2719c8671ac16e558a6b57eb
poohoh <kjh980328@gmail.com>
2023-06-20 12:17:53 +0900

    feat: AddOOTD, OOTD 추가 페이지

---
+++ MyCloset/src/pages/AddOOTD.jsx
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import './AddOOTD.css';
+import Mainbar from '../components/Mainbar/Mainbar';
+
+function AddOOTD() {
+  const [ootd, setOotd] = useState({
+    fullImage: '',
+    topImage: '',
+    bottomImage: '',
+    outerImage: ''
+  });
+
+  const history = useHistory();
+
+  const handleChange = (event) => {
+    setOotd({
+      ...ootd,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const storedOotds = JSON.parse(localStorage.getItem('ootds')) || [];
+    storedOotds.push(ootd);
+    localStorage.setItem('ootds', JSON.stringify(storedOotds));
+    alert('OOTD added!');
+    history.push('/ootd');
+  };
+
+  const handleCancel = () => {
+    history.goBack();
+  };
+
+  return (
+    <div>
+      <h1>Add OOTD</h1>
+      <Mainbar />
+      <form onSubmit={handleSubmit} className='ootd-form'>
+        <input 
+          className='input-field' 
+          name="fullImage" 
+          placeholder="Full Image URL" 
+          onChange={handleChange} 
+        />
+        <input 
+          className='input-field' 
+          name="topImage" 
+          placeholder="Top Image URL" 
+          onChange={handleChange} 
+        />
+        <input 
+          className='input-field' 
+          name="bottomImage" 
+          placeholder="Bottom Image URL" 
+          onChange={handleChange} 
+        />
+        <input 
+          className='input-field' 
+          name="outerImage" 
+          placeholder="Outer Image URL" 
+          onChange={handleChange} 
+        />
+        <div className='button-container'>
+          <button type="submit">Save</button>
+          <button type="button" onClick={handleCancel}>Cancel</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default AddOOTD;
